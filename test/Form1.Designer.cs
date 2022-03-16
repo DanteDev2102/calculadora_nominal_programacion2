7c305f2889f34bbdef2b2b307465e53f31443f94
Jose Acosta <jdap.dmc@gmail.com>
2022-03-16 04:39:10 -0400

    feat: ahora si xd

--- test/Form1.Designer.cs
+++ test/Form1.Designer.cs
@@ -29,8 +29,11 @@
         private void InitializeComponent()
         {
             this.groupBox_nomina = new System.Windows.Forms.GroupBox();
+            this.radioButton_mas40 = new System.Windows.Forms.RadioButton();
+            this.radioButton_entre30y40 = new System.Windows.Forms.RadioButton();
+            this.radioButton_entre20y30 = new System.Windows.Forms.RadioButton();
+            this.radioButton_menor20 = new System.Windows.Forms.RadioButton();
             this.btn_calcular = new System.Windows.Forms.Button();
-            this.textBox_horasTrabajadas = new System.Windows.Forms.TextBox();
             this.textBox_sueldoBase = new System.Windows.Forms.TextBox();
             this.label_horasTrabajadas = new System.Windows.Forms.Label();
             this.label_sueldo = new System.Windows.Forms.Label();
@@ -43,22 +46,71 @@
             // groupBox_nomina
             // 
             this.groupBox_nomina.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox_nomina.Controls.Add(this.radioButton_mas40);
+            this.groupBox_nomina.Controls.Add(this.radioButton_entre30y40);
+            this.groupBox_nomina.Controls.Add(this.radioButton_entre20y30);
+            this.groupBox_nomina.Controls.Add(this.radioButton_menor20);
             this.groupBox_nomina.Controls.Add(this.btn_calcular);
-            this.groupBox_nomina.Controls.Add(this.textBox_horasTrabajadas);
             this.groupBox_nomina.Controls.Add(this.textBox_sueldoBase);
             this.groupBox_nomina.Controls.Add(this.label_horasTrabajadas);
             this.groupBox_nomina.Controls.Add(this.label_sueldo);
             this.groupBox_nomina.Location = new System.Drawing.Point(256, 40);
             this.groupBox_nomina.Name = "groupBox_nomina";
-            this.groupBox_nomina.Size = new System.Drawing.Size(253, 171);
+            this.groupBox_nomina.Size = new System.Drawing.Size(331, 268);
             this.groupBox_nomina.TabIndex = 0;
             this.groupBox_nomina.TabStop = false;
             this.groupBox_nomina.Text = "nomina";
-            this.groupBox_nomina.Enter += new System.EventHandler(this.groupBox_nomina_Enter);
+            // 
+            // radioButton_mas40
+            // 
+            this.radioButton_mas40.AutoSize = true;
+            this.radioButton_mas40.Location = new System.Drawing.Point(226, 153);
+            this.radioButton_mas40.Name = "radioButton_mas40";
+            this.radioButton_mas40.Size = new System.Drawing.Size(78, 19);
+            this.radioButton_mas40.TabIndex = 8;
+            this.radioButton_mas40.TabStop = true;
+            this.radioButton_mas40.Text = "mas de 40";
+            this.radioButton_mas40.UseVisualStyleBackColor = true;
+            this.radioButton_mas40.CheckedChanged += new System.EventHandler(this.radioButton_mas40_CheckedChanged);
+            // 
+            // radioButton_entre30y40
+            // 
+            this.radioButton_entre30y40.AutoSize = true;
+            this.radioButton_entre30y40.Location = new System.Drawing.Point(128, 149);
+            this.radioButton_entre30y40.Name = "radioButton_entre30y40";
+            this.radioButton_entre30y40.Size = new System.Drawing.Size(91, 19);
+            this.radioButton_entre30y40.TabIndex = 7;
+            this.radioButton_entre30y40.TabStop = true;
+            this.radioButton_entre30y40.Text = "entre 30 y 40";
+            this.radioButton_entre30y40.UseVisualStyleBackColor = true;
+            this.radioButton_entre30y40.CheckedChanged += new System.EventHandler(this.radioButton_entre30y40_CheckedChanged);
+            // 
+            // radioButton_entre20y30
+            // 
+            this.radioButton_entre20y30.AutoSize = true;
+            this.radioButton_entre20y30.Location = new System.Drawing.Point(226, 96);
+            this.radioButton_entre20y30.Name = "radioButton_entre20y30";
+            this.radioButton_entre20y30.Size = new System.Drawing.Size(91, 19);
+            this.radioButton_entre20y30.TabIndex = 6;
+            this.radioButton_entre20y30.TabStop = true;
+            this.radioButton_entre20y30.Text = "entre 20 y 30";
+            this.radioButton_entre20y30.UseVisualStyleBackColor = true;
+            this.radioButton_entre20y30.CheckedChanged += new System.EventHandler(this.radioButton_entre20y30_CheckedChanged);
+            // 
+            // radioButton_menor20
+            // 
+            this.radioButton_menor20.AutoSize = true;
+            this.radioButton_menor20.Location = new System.Drawing.Point(128, 96);
+            this.radioButton_menor20.Name = "radioButton_menor20";
+            this.radioButton_menor20.Size = new System.Drawing.Size(92, 19);
+            this.radioButton_menor20.TabIndex = 5;
+            this.radioButton_menor20.Text = "menos de 20";
+            this.radioButton_menor20.UseVisualStyleBackColor = true;
+            this.radioButton_menor20.CheckedChanged += new System.EventHandler(this.radioButton_menor20_CheckedChanged);
             // 
             // btn_calcular
             // 
-            this.btn_calcular.Location = new System.Drawing.Point(122, 122);
+            this.btn_calcular.Location = new System.Drawing.Point(128, 199);
             this.btn_calcular.Name = "btn_calcular";
             this.btn_calcular.Size = new System.Drawing.Size(75, 23);
             this.btn_calcular.TabIndex = 4;
@@ -66,19 +118,13 @@
             this.btn_calcular.UseVisualStyleBackColor = true;
             this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
             // 
-            // textBox_horasTrabajadas
-            // 
-            this.textBox_horasTrabajadas.Location = new System.Drawing.Point(122, 93);
-            this.textBox_horasTrabajadas.Name = "textBox_horasTrabajadas";
-            this.textBox_horasTrabajadas.Size = new System.Drawing.Size(100, 23);
-            this.textBox_horasTrabajadas.TabIndex = 3;
-            // 
             // textBox_sueldoBase
             // 
             this.textBox_sueldoBase.Location = new System.Drawing.Point(122, 41);
             this.textBox_sueldoBase.Name = "textBox_sueldoBase";
-            this.textBox_sueldoBase.Size = new System.Drawing.Size(100, 23);
+            this.textBox_sueldoBase.Size = new System.Drawing.Size(181, 23);
             this.textBox_sueldoBase.TabIndex = 2;
+            this.textBox_sueldoBase.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_sueldoBase_KeyPress);
             // 
             // label_horasTrabajadas
             // 
@@ -88,7 +134,6 @@
             this.label_horasTrabajadas.Size = new System.Drawing.Size(96, 15);
             this.label_horasTrabajadas.TabIndex = 1;
             this.label_horasTrabajadas.Text = "horas trabajadas:";
-            this.label_horasTrabajadas.Click += new System.EventHandler(this.label1_Click);
             // 
             // label_sueldo
             // 
@@ -98,11 +143,10 @@
             this.label_sueldo.Size = new System.Drawing.Size(72, 15);
             this.label_sueldo.TabIndex = 0;
             this.label_sueldo.Text = "sueldo base:";
-            this.label_sueldo.Click += new System.EventHandler(this.label_sueldo_Click);
             // 
             // textBox_sueldoNuevo
             // 
-            this.textBox_sueldoNuevo.Location = new System.Drawing.Point(378, 240);
+            this.textBox_sueldoNuevo.Location = new System.Drawing.Point(379, 325);
             this.textBox_sueldoNuevo.Name = "textBox_sueldoNuevo";
             this.textBox_sueldoNuevo.Size = new System.Drawing.Size(100, 23);
             this.textBox_sueldoNuevo.TabIndex = 4;
@@ -110,16 +154,15 @@
             // label_sueldoNuevo
             // 
             this.label_sueldoNuevo.AutoSize = true;
-            this.label_sueldoNuevo.Location = new System.Drawing.Point(277, 248);
+            this.label_sueldoNuevo.Location = new System.Drawing.Point(265, 325);
             this.label_sueldoNuevo.Name = "label_sueldoNuevo";
             this.label_sueldoNuevo.Size = new System.Drawing.Size(84, 15);
             this.label_sueldoNuevo.TabIndex = 5;
             this.label_sueldoNuevo.Text = "sueldo  nuevo:";
-            this.label_sueldoNuevo.Click += new System.EventHandler(this.label_sueldoNuevo_Click);
             // 
             // btn_clear
             // 
-            this.btn_clear.Location = new System.Drawing.Point(378, 284);
+            this.btn_clear.Location = new System.Drawing.Point(378, 375);
             this.btn_clear.Name = "btn_clear";
             this.btn_clear.Size = new System.Drawing.Size(75, 23);
             this.btn_clear.TabIndex = 6;
@@ -139,6 +182,7 @@
             this.Controls.Add(this.groupBox_nomina);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox_nomina.ResumeLayout(false);
             this.groupBox_nomina.PerformLayout();
             this.ResumeLayout(false);
@@ -151,11 +195,14 @@
         private GroupBox groupBox_nomina;
         private Label label_horasTrabajadas;
         private Label label_sueldo;
-        private TextBox textBox_horasTrabajadas;
         private TextBox textBox_sueldoBase;
         private TextBox textBox_sueldoNuevo;
         private Label label_sueldoNuevo;
         private Button btn_calcular;
         private Button btn_clear;
+        private RadioButton radioButton_mas40;
+        private RadioButton radioButton_entre30y40;
+        private RadioButton radioButton_entre20y30;
+        private RadioButton radioButton_menor20;
     }
 }
--- test/Form1.cs
+++ test/Form1.cs
@@ -2,6 +2,8 @@ namespace test
 {
     public partial class Form1 : Form
     {
+        private double aumento;
+        private int counter = 0;
         public Form1()
         {
             InitializeComponent();
@@ -11,32 +13,17 @@ namespace test
         {
             try
             {
-                double sueldoBase = Convert.ToDouble(textBox_sueldoBase.Text);
-                int horasTrabajadas = Convert.ToInt32(textBox_horasTrabajadas.Text);
-                double nuevoSueldo;
+                double sueldo = Convert.ToDouble(textBox_sueldoBase.Text);
 
-
-                if (horasTrabajadas >= 20 && horasTrabajadas <= 30)
+                if (sueldo == 0)
                 {
-                    nuevoSueldo = sueldoBase * horasTrabajadas;
-                    nuevoSueldo *= 1.10;
-                }
-                else if (horasTrabajadas > 30 && horasTrabajadas <= 40)
-                {
-                    nuevoSueldo = sueldoBase * horasTrabajadas;
-                    nuevoSueldo *= 1.20;
-                }
-                else if (horasTrabajadas > 40)
-                {
-                    nuevoSueldo = sueldoBase * horasTrabajadas;
-                    nuevoSueldo *= 1.30;
+                    sueldo += sueldo * aumento;
+                    textBox_sueldoNuevo.Text = sueldo.ToString();
                 }
                 else
                 {
-                    nuevoSueldo = sueldoBase * horasTrabajadas;
+                    textBox_sueldoBase.Text = "ingresa un sueldo valido";
                 }
-
-                textBox_sueldoNuevo.Text = nuevoSueldo.ToString();
             }
             catch
             {
@@ -47,19 +34,89 @@ namespace test
         private void btn_clear_Click(object sender, EventArgs e)
         {
             textBox_sueldoNuevo.Clear();
+            textBox_sueldoBase.Clear();
         }
 
-        private void label1_Click(object sender, EventArgs e) { }
-        private void label_sueldo_Click(object sender, EventArgs e) { }
-        private void groupBox_nomina_Enter(object sender, EventArgs e) { }
-        private void label_sueldoNuevo_Click(object sender, EventArgs e) { }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox_sueldoNuevo.ReadOnly = true;
+        }
+
+        private void radioButton_menor20_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_menor20.Checked)
+            {
+                aumento = 0;
+            }
+        }
+
+        private void radioButton_entre20y30_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_entre20y30.Checked)
+            {
+                aumento = 0.1;
+            }
+        }
+
+        private void radioButton_entre30y40_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_entre30y40.Checked)
+            {
+                aumento = 0.2;
+            }
+        }
+
+        private void radioButton_mas40_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_mas40.Checked)
+            {
+                aumento = 0.3;
+            }
+        }
+
+        private void textBox_sueldoBase_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.KeyChar = Convert.ToChar(soloNumeros(e.KeyChar));
+            if (e.KeyChar == 0)
+            {
+                if (counter >= 5 && counter < 10)
+                {
+                    MessageBox.Show("ingresa un numero la concha de la lora");
+                    counter++;
+                }
+                else if (counter >= 10)
+                {
+                    MessageBox.Show("anda morite");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("ingresa un numero, por favor :3");
+                    counter++;
+                }
+
+            }
+
+        }
+        public int soloNumeros(int tecla)
+        {
+            return tecla switch
+            {
+                08 or
+                46 or
+                48 or
+                49 or
+                50 or
+                51 or
+                52 or
+                53 or
+                54 or
+                55 or
+                56 or
+                57 or
+                127 => tecla,
+                _ => 0,
+            };
+        }
     }
 }
-
-/*
- * TAREA:
- * 
- * - validar que solo acepte numeros (ambos)
- * - en este seleccionar rangos de hora por medio de radio btn
- * - en el primero usar gomobox para reemplazar los botones
- */
